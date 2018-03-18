d67492346198b082acc4c09de4b21e68532947af
18392192892 <18392192892@163.com>
2018-03-18 19:05:58 +0800

    合并

--- pc/js/page1.js
+++ pc/js/page1.js
@@ -1,4 +1,5 @@
 let page1 = {
+
 	getEle: function() {
 		let goDown = document.getElementById('page1-goDown');
 		goDown.onclick = function() {
@@ -6,9 +7,19 @@ let page1 = {
 	    }
 	},
 
+	tips: function() {
+		if(loginEnroll.loginFlag) {
+			layer.tips('点我退出登录', '#loginP', {
+		  		tips: [1, '#F061B3'],
+		  		time: 2000
+			});
+		}
+	},
+
 	init: function() {
 		this.getEle();
 	}
+
 }
 
 window.page1 = page1;
